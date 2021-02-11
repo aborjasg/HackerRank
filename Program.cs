2c4aa833df631413a31251fda074bbe57b9c452b
Alex BG <aborjasg@outlook.com>
2021-02-11 12:51:39 -0800

    2021-02-11 More functions

--- Program.cs
+++ Program.cs
@@ -9,6 +9,153 @@ namespace HackerRank
 {
     class Program
     {
+        #region "Functions"
+
+        static int minimumSwaps(int[] arr) {
+            int swaps=0; 
+            List<int> a = arr.ToList();
+
+            Console.WriteLine("Start: {0}", string.Join( ",", arr.ToArray()) );
+            
+            for (int p=0; p < a.Count-1; p++) {
+
+                if (a[p] != p + 1) {
+
+                    int temp = a[p];
+                    int target = a.FindIndex(p+1, x => x == p+1);
+                    a[p] = a[target];                                                                   
+                    a[target] = temp;
+                    swaps++;   
+                    Console.WriteLine("[{0}] -> [{1}] => ({2})", p, target, string.Join( ",", a.ToArray()) );        
+                }                                   
+            }
+            return swaps;
+        }
+
+        static long arrayManipulation(int n, int[][] queries) {
+            var res = new List<int>(new int[n]);
+            
+            Console.WriteLine("Array: {0}", string.Join(",", res.ToArray()));
+
+            for (int p=0; p < queries.Length; p++) {
+                Console.WriteLine("[{0}]-[{1}] / Add: {2}", queries[p][0], queries[p][1], queries[p][2]);
+
+                for (int q=queries[p][0]-1; q<queries[p][1]; q++) {
+                    res[q] += queries[p][2];
+                }
+                Console.WriteLine("queries: {0}", string.Join(",", res.ToArray()));
+            }
+            
+            return res.Max();
+        }
+
+        static int missingInteger (int[] A)
+        {
+            int res = 0;
+            int max = 0;
+            for (int k=0; k < A.Length; k++) {
+                int ind = Array.FindIndex(A, x => x == k+1);
+                Console.WriteLine("ind={0}", ind);
+                if (ind == -1) {
+                    res = k+1;
+                    break;
+                }
+                else {
+                    res = k+2;
+                }
+                    
+                if (max < A[k])
+                    max = A[k];
+                
+            }
+            if (max ==0)
+                    res = 1;
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            return res;
+        }
+
+        static int sherlockAndAnagrams(string s) {
+            int res = 0;
+            if (s.Length > 1 && s.Length <= 100) {
+                
+                Console.WriteLine("s={0}", s);
+                for(int p=1; p < s.Length; p++) {
+                    for(int q = 0; q < s.Length - p; q++) {                        
+                        var str1 = new string(s.Substring(q, p).OrderBy(x => x).ToArray());                        
+                        string s1 = s.Substring(q+1);
+                        /*
+                        for (int k=0; k < s1.Length - p + 1; k++) {
+                            var str2 = s1.Substring(k, p).OrderBy(x => x);                            
+
+                            if (str1.SequenceEqual(str2)) {                                                                          
+                                res++;
+                                //Console.WriteLine("str1={0} / s1={1} / str2={2} / k={3}", s.Substring(q, p), s1, s1.Substring(k, p), k); 
+                            }   
+                        }*/
+                        var list = new List<string>();
+                     
+                        for (int k=0; k < s1.Length - p + 1; k++) {
+                            var str2 = new string(s1.Substring(k, p).OrderBy(x => x).ToArray());   
+                            //Console.WriteLine("str2={0}", s1.Substring(k, p)); 
+                            list.Add(str2);
+                            //Console.WriteLine("str1={0} / s1={1} / str2={2} / list={3}", s.Substring(q, p), s1, s1.Substring(k, p), list.Count);  
+                        }                    
+                        res += list.Count(x => x == str1);
+                        //Console.WriteLine("count={0}", list.Count(x => x == str1));
+                    }                
+                }
+            }
+            return res;
+        }
+
+        static string isValid(string s) {
+            string res="NO";
+            var dict = new Dictionary<char, int>();
+            
+            for(int k = 0; k < s.Length; k++) {
+                if (!dict.ContainsKey(s[k])) {
+                    dict.Add(s[k], 1);
+                }
+                else {
+                    dict[s[k]]++;
+                }
+            }
+            
+            foreach(KeyValuePair<char, int> item in dict) {
+                Console.WriteLine("Key: {0}, Value: {1}", item.Key, item.Value);
+            } 
+            
+            var diffChar = dict.Keys.Distinct();
+            int maxNumber = dict.Values.Max();
+            int maxCount = dict.Count(x => x.Value == maxNumber);
+            int minNumber = dict.Values.Min();
+            int minCount = dict.Count(x => x.Value == minNumber);
+            
+            Console.WriteLine("diff_Char={0} / maxNumber={1} / maxCount={2} / maxNumber={3} / minCount={4}", diffChar.Count(), maxNumber, maxCount, minNumber, minCount);
+
+            if ( diffChar.Count() - maxCount < 2 || ((maxCount == 1 || minCount == 1 ) && (maxNumber - minNumber <= 1)) ){
+                res = "YES";
+            }
+
+            return res;
+        }
+
+        static long substrCount(int n, string s) {
+            int res = n;
+            
+            for (int p=2; p <= 3; p++) {
+                for (int q=0; q < s.Length - p + 1; q++) {
+                    string str = s.Substring(q, p);
+                                    
+                    if (str.PadLeft(str.Length/2).ToList().SequenceEqual(str.PadRight(str.Length/2).Reverse())) {
+                        res++;
+                        //Console.WriteLine("str={0}", str);
+                    }
+                }
+            }
+            return res;
+        }
+
         static long repeatedString(string s, long n) {
             long l = Convert.ToInt32(n / s.Length);
             string s1 = string.Empty;
@@ -118,8 +265,11 @@ namespace HackerRank
             return res.ToString();
         }
         
+        #endregion
+        
         static void Main(string[] args)
         {
+            #region "Previous calls"
             /*
             string s="aba";
             long n = 10;
@@ -144,6 +294,9 @@ namespace HackerRank
             int k = 62;
             Console.WriteLine("{0}", caesarCipher(str, k));
             */
+            #endregion
+
+            
         }
     }
 }
